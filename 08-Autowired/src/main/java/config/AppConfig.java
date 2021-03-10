7f1fcdf9011c779534b7278333054fc3cfff4711
易星科（AV21514） <AV21514@av-display.com>
2021-03-10 17:37:23 +0800

    spring自动装配

---
+++ 08-Autowired/src/main/java/config/AppConfig.java
@@ -0,0 +1,18 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @ClassName: AppConfig
+ * @Description: 配置文件
+ * @Author ONESTAR
+ * @微信：YXK-ONESTAR
+ * @URL：https://onestar.newstar.net.cn/
+ * @Date: 2021/3/10 16:43
+ * @Version 1.0
+ */
+@Configuration
+public class AppConfig {
+
+
+}
---
+++ 08-Autowired/src/main/java/controller/DogController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @ClassName: DogController
+ * @Description: TODO
+ * @Author ONESTAR
+ * @微信：YXK-ONESTAR
+ * @URL：https://onestar.newstar.net.cn/
+ * @Date: 2021/3/10 17:14
+ * @Version 1.0
+ */
+@Controller
+public class DogController {
+}
