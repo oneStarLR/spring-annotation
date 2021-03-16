24a232420797ce9a7fb7b9ff47f1f8f8a0624995
易星科（AV21514） <AV21514@av-display.com>
2021-03-16 17:40:50 +0800

    完善@Profile注解案例

--- 10-Profile/src/main/java/config/AppConfig.java
+++ 10-Profile/src/main/java/config/AppConfig.java
@@ -17,17 +17,19 @@ import java.beans.PropertyVetoException;
  * @Date: 2021/3/15 16:41
  * @Version 1.0
  */
+// @Profile("test")
 @Configuration
 public class AppConfig {
 
     // 测试环境
+    // @Profile("default")
     @Profile("test")
     @Bean("testDataSource")
     public DataSource dataSourceTest() throws PropertyVetoException {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setUser("root");
-        dataSource.setPassword("806188");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/vhr");
+        dataSource.setPassword("123");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/test");
         dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
         return dataSource;
     }
@@ -38,8 +40,8 @@ public class AppConfig {
     public DataSource dataSourceDev() throws PropertyVetoException {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setUser("root");
-        dataSource.setPassword("806188");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/vhr");
+        dataSource.setPassword("456");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/dev");
         dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
         return dataSource;
     }
@@ -50,8 +52,8 @@ public class AppConfig {
     public DataSource dataSourcePro() throws PropertyVetoException {
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setUser("root");
-        dataSource.setPassword("806188");
-        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/vhr");
+        dataSource.setPassword("789");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/pro");
         dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
         return dataSource;
     }
--- 10-Profile/src/test/java/MainTest.java
+++ 10-Profile/src/test/java/MainTest.java
@@ -17,7 +17,14 @@ public class MainTest {
     @Test
     public void TestMain() {
         // 创建IOC容器
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        // 设置需要激活的环境
+        applicationContext.getEnvironment().setActiveProfiles("pro");
+        // 注册主配置类
+        applicationContext.register(AppConfig.class);
+        // 启动刷新容器
+        applicationContext.refresh();
         String[] namesForType = applicationContext.getBeanNamesForType(DataSource.class);
         for (String string : namesForType) {
             System.out.println(string);
